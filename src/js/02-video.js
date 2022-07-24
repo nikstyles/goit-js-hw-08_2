5169ff93cdd0f85922b51dded1ecf6f6f340be97
Nikita <nicstyle22@gmail.com>
2022-07-24 09:57:57 +0300

    add 2 task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,11 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('timeupdate', throttle(onTimeSave, 1000));
+function onTimeSave(timeupdate) {
+  console.log(timeupdate.seconds);
+  localStorage.setItem('videoplayer-current-time', timeupdate.seconds);
+}
